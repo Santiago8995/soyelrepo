e0cad6d4ba23f7a322f7043612f326595ce9594d
Santiago Pineiro <santiago.8.9.95@gmail.com>
2021-04-06 19:42:06 -0300

    comienzo del proyecto

---
+++ src/main/java/com/empresa/sistema/App.java
@@ -0,0 +1,14 @@
+package com.empresa.sistema;
+
+/**
+ * @author Santiago
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Soy un Mensaje de App.Java");
+	}
+
+}
