19599293914a507ab1205eb9c1a73c2332b8ff76
Santiago8995 <72951889+Santiago8995@users.noreply.github.com>
2021-04-06 19:49:40 -0300

    Modificacion en Git
    syso("Soy una modificacion en GIT");

--- src/main/java/com/empresa/sistema/App.java
+++ src/main/java/com/empresa/sistema/App.java
@@ -9,6 +9,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Soy un Mensaje de App.Java");
+		System.out.println("Soy una modificacion en GIT");
 	}
 
 }
